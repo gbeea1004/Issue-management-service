af1e8a672b3daa407b4134216de0829d438a14e6
geonHee <corqkrtk3@naver.com>
2020-04-10 14:52:45 +0900

    Initial project

---
+++ src/main/java/com/gunny/renewims/RenewImsApplication.java
@@ -0,0 +1,13 @@
+package com.gunny.renewims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunny/renewims/RenewImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunny.renewims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenewImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
